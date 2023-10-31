8b2d42d388d19970f2898d715c96493163fa50ef
DucHuy2801 <theflash28012002@gmail.com>
2023-10-31 19:04:47 +0700

    commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import './Navbar.css'
 
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
 const Navbar = () => {
     return (
         <div className="navbar">
-
+            <div className="nav-logo">
+                <img src={logo} alt="" />
+                <p>CLOTHES SHOP</p>
+            </div>
+            <ul className="nav-menu">
+                <li>Shop</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>Kids</li>
+            </ul>
         </div>
     )
 }

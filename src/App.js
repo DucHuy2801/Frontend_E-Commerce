9d0e76ec3f8aa6fe5ebc156ae0605e0894492fe9
DucHuy2801 <theflash28012002@gmail.com>
2023-11-02 15:39:05 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart'
+import LoginSignup from './Pages/LoginSignup'
 
 function App() {
   return (
@@ -10,12 +13,15 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Shop />} />
+          <Route path='/shop' element={<Shop />} />
           <Route path='/men' element={<ShopCategory category="men"/>} />
           <Route path='/women' element={<ShopCategory category="women" />} />
           <Route path='/kids' element={<ShopCategory category="kids" />} />
-
-          
+          <Route path='/product' element={<Product />} >
+            <Route path=':productId' element={<Product />} />
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
         </Routes>
       </BrowserRouter>
     </div>

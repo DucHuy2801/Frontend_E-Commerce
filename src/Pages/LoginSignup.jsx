ee5160ff02304ba5dbfe0b64891bc1b44d63a425
DucHuy2801 <theflash28012002@gmail.com>
2023-11-06 21:38:23 +0700

    commit

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const LoginSignup = () => {
     return (
-        <div>
-
+        <div className="loginsignup">
+            <div className="loginsignup-container">
+                <h1>Sign Up</h1>
+                <div className="loginsignup-fields">
+                    <input type="text" placeholder="Your Name" />
+                    <input type="email" placeholder="Email Address" />
+                    <input type="password" placeholder="Password" />
+                </div>
+                <button>Continue</button>
+                {/* <p className="loginsignup-login"> Already have an account? <span>Login here<span/></p> */}
+            </div>
         </div>
     )
 }
